96d3d4c85e4f1206eff9a7b49f6e3348ffec950a
paldepind <simonfv@gmail.com>
2015-04-09 11:28:10 +0200

    Curry

--- sampleon.js
+++ sampleon.js
@@ -1,7 +1,7 @@
 var flyd = require('flyd');
 
-module.exports = function(s1, s2) {
+module.exports = flyd.curryN(2, function(s1, s2) {
   return flyd.stream([s1], function() {
     return s2.val;
   });
-};
+});
